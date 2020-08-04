b3a0dcf502c424a6e22dda10d27677247522d82a
Alek <alekvalencia2535@gmail.com>
2020-08-03 22:58:31 -0700

    Added server.js and set it up to run. Added routes folder with api- and html-routes.js files. Html-routes is set up while api-routes is partially setup. Also added index.js file models folder set export Workout model.

---
+++ routes/api-routes.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const mongoose = require("mongoose");
+const db = require("../models");
+
+router.get("/api/workouts", (req, res) => {
+
+});
+
+router.get("/api/workouts/range", (req, res) => {
+
+});
+
+router.post("/api/workouts", (req, res) => {
+
+});
+
+router.post("/api/workouts/:id", (req, res) => {
+
+});
